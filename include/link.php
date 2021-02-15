8b253ac1b7d62ae2a45100029ca7b225445500df
imvishalnag <imvishalnag@gmail.com>
2021-02-15 17:48:23 +0530

    u

--- include/link.php
+++ include/link.php
@@ -1,6 +1,6 @@
                 <li><a href="index.php">Home</a></li>
-                <li><a href="About_us.html">About Us</a></li>
-                <li><a href="news.html">News</a></li>
+                <li><a href="About_us.php">About Us</a></li>
+                <li><a href="news.php">News</a></li>
                 <li><a href="donation.php">Donation</a></li>
                 <li><a href="gallery.php">Gallery</a></li>
                 <li><a href="certificate.php">Certificate</a></li>
